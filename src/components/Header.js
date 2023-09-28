9b906e506095cddcb8c6a34e22aa0323480621c9
Hopkomax <gopkomax@gmail.com>
2023-09-29 01:57:56 +0300

    updated paths

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header(props) {
 
   return (
     <header className="d-flex justify-between align-center p-40">
-      <Link to="">
+      <Link to="/">
         <div className="d-flex align-center">
           <img width={40} height={40} src="img/logo.png" alt="Logo" />
           <div className="headerInfo">
@@ -21,13 +21,13 @@ function Header(props) {
           <img width={18} height={18} src="img/cart.svg" alt="Cart" />
           <span>{totalPrice}$</span>
         </li>
-        <Link to="favorites">
-          <li className="mr-20 cu-p">
+        <li className="mr-20 cu-p">
+          <Link to="/favorites">
             <img width={18} height={18} src="img/heart.svg" alt="Heart" />
-          </li>
-        </Link>
+          </Link>
+        </li>
         <li>
-          <Link to="orders">
+          <Link to="/orders">
             <img width={18} height={18} src="img/user.svg" alt="User" />
           </Link>
         </li>
